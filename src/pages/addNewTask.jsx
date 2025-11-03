2c48b8cd4cccc2802db096089fc59476e978233e
Igor Montovani <igor.montovani@gmail.com>
2025-11-03 21:50:45 +0100

    Add form logic and add to database

--- src/pages/addNewTask.jsx
+++ src/pages/addNewTask.jsx
@@ -1,26 +1,59 @@
+import { useState } from "react";
 import TitleAndDescription from "../components/TitleAndDescription";
 
-function AddNewTask() {
+function AddNewTask({taskData,setTaskData}) {
+    const [formData,setFormData] = useState({
+        id:Math.floor(Math.random()*10000).toString(),
+        title:"",
+        description: "",
+        assignee: "",
+        status: "",
+        priority:"",
+        createdDate: new Date().toISOString().split('T')[0],
+        dueDate: "",
+    }
+    )
+    function handleChange(e){
+        const {name,value} = e.target
+        setFormData(prev=>({...prev,[name] : value}))
+    }
+    function handleAddTaskData(e){
+        e.preventDefault()
+        setTaskData(prev=>([...prev,formData]))
+        
+    }
+    const formStyle = {
+        display:'flex',
+        flexDirection: 'column',
+        gap: '20px',
+    }
+    const divFormStyle = {
+        backgroundColor:'#14161990',
+        margin: '30px',
+        padding: '20px',
+        borderRadius: '20px'
+    }
+    console.log(taskData)
   return (
     <>
       <TitleAndDescription title={'Create a new Task'} description={"Add all task's details"}/>
-      <div>
-            <form>
+      <div style={divFormStyle}>
+            <form style={formStyle}>
             <label>
                 Name:
-                <input name='title' type="text" placeholder="Task Title" ></input>
+                <input name='title' type="text" placeholder="Task Title"  onChange={handleChange} value={formData.title}></input>
             </label>
             <label>
                 Description:
-                <input name ='description' type="text" placeholder="Task Description"/>
+                <input name ='description' type="text" placeholder="Task Description" onChange={handleChange} value={formData.description}/>
             </label>
             <label>
                 Assignee:
-                <input name='assignee' type="text" placeholder="Eg. John" />
+                <input name='assignee' type="text" placeholder="Eg. John" onChange={handleChange} value={formData.assignee} />
             </label>
             <label>
                 Status:
-                <select name="status" >
+                <select name="status" onChange={handleChange} value={formData.status} >
                     <option value="">-- None --</option>
                     <option value="To Do"> To Do </option>
                     <option value="In Progress"> In Progress</option>
@@ -29,7 +62,7 @@ function AddNewTask() {
             </label>
             <label>
                 Priority:
-                <select name="priority" >
+                <select name="priority" onChange={handleChange} value={formData.priority} >
                     <option value="">-- None --</option>
                     <option value="Low"> Low </option>
                     <option value="Medium"> Medium</option>
@@ -38,8 +71,9 @@ function AddNewTask() {
             </label>
             <label>
                 Due Date:
-                <input type="date" name="duedate" />
+                <input type="date" name="dueDate" onChange={handleChange} value={formData.dueDate}/>
             </label>
+            <button type="submit"onClick={handleAddTaskData}>Create Task</button>
         </form>
         </div>
     </>
